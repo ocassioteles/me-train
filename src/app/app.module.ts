ea3c62efece741a6f543492ea6b7fda2ab84e284
ocassioteles <cassio.teles@live.com>
2025-04-03 14:00:46 -0300

    gerando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExerciseListComponent } from './exercise-list/exercise-list.component';
+import { ExerciseFormComponent } from './exercise-form/exercise-form.component';
+import { ExerciseDetailComponent } from './exercise-detail/exercise-detail.component';
+import { SeriesDetailComponent } from './series-detail/series-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExerciseListComponent,
+    ExerciseFormComponent,
+    ExerciseDetailComponent,
+    SeriesDetailComponent
   ],
   imports: [
     BrowserModule,
